c93fad18d0baa9a9c59f1d6caf2da91bb2c75647
Juls-m <143052615+Juls-m@users.noreply.github.com>
2024-04-30 15:31:04 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/attendancemanagement/AttendanceManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.attendancemanagement;
+
+/**
+ *
+ * @author Client
+ */
+public class AttendanceManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
